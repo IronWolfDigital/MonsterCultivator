617c3b186f0c21f7e601fb583969dddb471ee387
MatasV <matusisga@gmail.com>
2023-02-04 16:09:13 +0200

    Added third jar (no parts yet)

--- Assets/MonsterPartData.cs
+++ Assets/MonsterPartData.cs
@@ -12,6 +12,12 @@ public class MonsterPartData : MonoBehaviour
     public int currentPrice;
     public PartObjectName partObjectName;
     public TMP_Text costDisplay;
+
+    public bool isLocked;
+    public int unlockPrice;
+    public Sprite lockedSprite;
+    public Sprite unlockedSprite;
+    public SpriteRenderer spriteRenderer;
     private void OnEnable()
     {
         currentPrice = initialPrice;
@@ -20,11 +26,27 @@ public class MonsterPartData : MonoBehaviour
     private void OnMouseDown()
     {
         Debug.Log("cLICKED");
+
+        if (isLocked)
+        {
+            if (PurchaserController.Instance.soulsCount >= unlockPrice)
+            {
+                PurchaserController.Instance.soulsCount -= unlockPrice;
+                isLocked = false;
+                spriteRenderer.sprite = unlockedSprite;
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+        
         PurchaserController.Instance.TryPurchasingObjectPart(this);
     }
 
     private void Update()
     {
-        costDisplay.text = currentPrice.ToString();
+        costDisplay.text = isLocked ? "Unlock \n" + unlockPrice.ToString() : currentPrice.ToString();
     }
 }
