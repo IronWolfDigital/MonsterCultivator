4cc4c2f1ea3ad36252a8b8f730867b60e7035225
S.Adas <67442322+AdasStp@users.noreply.github.com>
2023-02-05 01:29:51 +0200

    MainScene v2

--- Assets/Scripts/ChangeImageSpriteOnHover.cs
+++ Assets/Scripts/ChangeImageSpriteOnHover.cs
@@ -2,20 +2,36 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class ChangeButtonSpriteOnHover : MonoBehaviour
+public class ChangeImageSpriteOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Button button;
     public Image image;
     public Sprite normalSprite;
     public Sprite hoverSprite;
-    PointerEventData cachedEventData;
+    //PointerEventData cachedEventData;
 
     private void OnPointerEnter(PointerEventData eventData)
     {
         var spriteState = new SpriteState();
         spriteState.highlightedSprite = hoverSprite;
         button.spriteState = spriteState;
-        cachedEventData = eventData;
+        //cachedEventData = eventData;
+    }
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        var spriteState = new SpriteState();
+        spriteState.highlightedSprite = hoverSprite;
+        button.spriteState = spriteState;
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        var spriteState = new SpriteState();
+        spriteState.highlightedSprite = hoverSprite;
+        button.spriteState = spriteState;
     }
 
     private void OnPointerExit(PointerEventData eventData)
@@ -23,7 +39,7 @@ public class ChangeButtonSpriteOnHover : MonoBehaviour
         var spriteState = new SpriteState();
         spriteState.highlightedSprite = normalSprite;
         button.spriteState = spriteState;
-        cachedEventData = eventData;
+        //cachedEventData = eventData;
     }
 
     private void Update()
